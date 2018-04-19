cef2adc5a66d485cbc516bf07500367fc43e9865
WaximeA <aveline.maxime@gmail.com>
2018-04-19 08:25:28 +0200

    #edit : Change edit button layout

--- resources/views/categories/category.blade.php
+++ resources/views/categories/category.blade.php
@@ -8,20 +8,17 @@
                 <p>{{ $category->description }}</p>
             </div>
             <div class="col-md-2">
-                <a href="{{ route('edit_category', $category->id) }}" class="btn btn-primary"><span style="float: right">edit</span></a>
+                <a href="{{ route('edit_category', $category->id) }}" class="btn btn-primary btn-sm"><span style="float: right">edit</span></a>
             </div>
         </div>
         <hr>
-        <div class="row justify-content-center">
-            @include('topics/add_topic')
-        </div>
         <div class="row">&nbsp;</div>
         @foreach($topics as $topic)
             <div class="row justify-content-center">
                 <div class="col-md-8">
                     <div class="card">
                         <div class="card-header">
-                            <span><b>{{ $topic->title }}</b><a href="{{ route('edit_topic', $topic->id) }}"><span style="float: right">edit</span></a></span>
+                            <span><b>{{ $topic->title }}</b><a href="{{ route('edit_topic', $topic->id) }}"><span class="btn btn-primary btn-sm" style="float: right">edit</span></a></span>
                         </div>
                         <div class="card-body">
                             <p class="card-text">{{ $topic->content }}</p>
@@ -32,6 +29,9 @@
             </div>
             <div class="row">&nbsp;</div>
         @endforeach
+        <div class="row justify-content-center">
+            @include('topics/add_topic')
+        </div>
         <a href="{{ route('categories') }}" class="btn btn-primary">Return</a>
     </div>
 @endsection
