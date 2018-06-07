dc7d78fe18b55bab7b430f88f73d98109e042d73
WaximeA <aveline.maxime@gmail.com>
2018-06-07 22:50:13 +0200

    #roles : Create user table seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use App\Role;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Retrive all roles
+        $administratorRole = Role::where('name', 'administrator')->first();
+        $moderatorRole  = Role::where('name', 'moderator')->first();
+        $memberRole  = Role::where('name', 'member')->first();
+
+        // Add administrator user
+        $employee = new User();
+        $employee->name = 'administrator';
+        $employee->email = 'administrator@administrator.fr';
+        $employee->password = bcrypt('administrator');
+        $employee->save();
+        $employee->roles()->attach($administratorRole);
+
+        // Add moderator user
+        $manager = new User();
+        $manager->name = 'moderator';
+        $manager->email = 'moderator@moderator.fr';
+        $manager->password = bcrypt('moderator');
+        $manager->save();
+        $manager->roles()->attach($moderatorRole);
+
+        // Add member user
+        $manager = new User();
+        $manager->name = 'member';
+        $manager->email = 'member@member.fr';
+        $manager->password = bcrypt('member');
+        $manager->save();
+        $manager->roles()->attach($memberRole);
+    }
+}
