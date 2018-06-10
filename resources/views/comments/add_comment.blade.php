1ff4f85217b09bd44b36eff7581cab3c0a25cd6b
WaximeA <aveline.maxime@gmail.com>
2018-06-10 20:59:00 +0200

    #uploading-file : Add field inputs in forms

--- resources/views/comments/add_comment.blade.php
+++ resources/views/comments/add_comment.blade.php
@@ -1,10 +1,14 @@
 <div class="col-md-8">
     <h1>Add a comment</h1>
-    {!! Form::open(array('url' => '/add-comment/submit-add-comment')) !!}
+    {!! Form::open(['url' => '/add-comment/submit-add-comment', 'files' => true]) !!}
     {{ Form::hidden('topic_id', $topic->id) }}
     <div class="form-group">
         {{ Form::label('content', 'Comment content') }}
         {{ Form::textArea('content', '', ['class'=>'form-control', 'placeholder'=>'Write what you want to share']) }}
+    </div>
+    <div class="form-group">
+        {!! Form::label('Add comment image') !!}
+        {!! Form::file('comment_image') !!}
     </div>
     <div>
         {{ Form::submit('Submit', ['class'=>'btn btn-success btn-lg float-right']) }}
